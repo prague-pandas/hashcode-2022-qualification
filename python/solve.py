030a9ff42dba966af25143cf532a814681d07bb1
Vojtech Rozhon <rozhovoj@fit.cvut.cz>
2022-02-24 21:56:50 +0100

    add for loop

--- python/solve.py
+++ python/solve.py
@@ -24,33 +24,41 @@ def solve(dataset, rng=None):
     schedule = sorted(projects, key=lambda name: score(name))
     solution = []
     cont_free = {name: 0 for name in contributors}
-    for name in schedule:
-        project = projects[name]
-        project_assignment = []
-        project_start = 0
-        length = project['num_days_to_complete']
-        assigned = set()
-        for skill_name, level in project['skills']:
-            j = skills.index(skill_name)
-            eligible = conts[:, j] >= level
-            eligible = np.nonzero(eligible)[0]
-            eligible = [e for e in eligible if e not in assigned]
-            if len(eligible) == 0:
-                break
-            c = rng.choice(eligible)
-            c_name = c_names[c]
-            project_assignment.append(c_name)
-            project_start = max(project_start, cont_free[c_name])
-            assigned.add(c)
-        if len(project_assignment) < len(project['skills']):
-            continue
-        project_end = project_start + length
-        if project_end <= project['best_before'] + project['score']:
-            solution.append((name, project_assignment))
-            for c_name, (skill_name, level) in zip(project_assignment, project['skills']):
-                cont_free[c_name] = project_end
+    found = True
+    finished = []
+    while found:
+        found = False
+        for name in schedule:
+            if name in finished:
+                continue
+            project = projects[name]
+            project_assignment = []
+            project_start = 0
+            length = project['num_days_to_complete']
+            assigned = set()
+            for skill_name, level in project['skills']:
                 j = skills.index(skill_name)
-                c = c_names.index(c_name)
-                if conts[c, j] <= level:
-                    conts[c, j] += 1
+                eligible = conts[:, j] >= level
+                eligible = np.nonzero(eligible)[0]
+                eligible = [e for e in eligible if e not in assigned]
+                if len(eligible) == 0:
+                    break
+                c = rng.choice(eligible)
+                c_name = c_names[c]
+                project_assignment.append(c_name)
+                project_start = max(project_start, cont_free[c_name])
+                assigned.add(c)
+            if len(project_assignment) < len(project['skills']):
+                continue
+            project_end = project_start + length
+            if project_end <= project['best_before'] + project['score']:
+                solution.append((name, project_assignment))
+                found = True
+                finished.append(name)
+                for c_name, (skill_name, level) in zip(project_assignment, project['skills']):
+                    cont_free[c_name] = project_end
+                    j = skills.index(skill_name)
+                    c = c_names.index(c_name)
+                    if conts[c, j] <= level:
+                        conts[c, j] += 1
     return solution
