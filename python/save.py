88eed46114fe6d21fe41b26c7626045b9c9526f2
Filip Bartek <filip.bartek@hotmail.com>
2022-02-24 19:43:59 +0100

    Rename a va

--- python/save.py
+++ python/save.py
@@ -1,10 +1,10 @@
-def save(schedule, f):
+def save(solution, f):
     """
     Schedule is a list of projects. Each project is a tuple:
     1. Project name
     2. List of names of people assigned
     """
-    f.write(f'{len(schedule)}\n')
-    for project in schedule:
+    f.write(f'{len(solution)}\n')
+    for project in solution:
         f.write(f'{project[0]}\n')
         f.write('%s\n' % ' '.join(project[1]))
